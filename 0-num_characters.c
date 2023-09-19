830cd4fecad59836dee074d31f3efe8269341a22
Jadline <njerijadline@gmail.com>
2023-09-18 23:09:07 -0700

    function that produces output according to a format.

--- 0-num_characters.c
+++ 0-num_characters.c
@@ -1,53 +1,41 @@
 #include "main.h"
-#include <stdarg.h>
-#include <stddef.h>
 /**
- * num_chars -  a vadriac function that takes a variable number of arguments
- * @format: pointer to a constant character
+ * printf - a variadic function that takes in variable arguments
  *
- * Return: returns the number of characters
+ * Return:returns the number of characters
  */
 int _printf(const char *format, ...)
 {
-	int i, my_chars, length_of_string;
-
-	va_list args;
-
+	int my_chars,k, length_of_string;
+	va_list num_of_args;
 	if (format == NULL)
-	{
 		return (-1);
-	}
-	va_start(args, format);
-	my_chars = 0;
-	for (i = 0; format[i] != '\0'; i++)
+	va_start(num_of_args,format);
+	for(k = 0; format[k] != '\0'; k++)
 	{
-		if (format[i] != '%')
+		if (format[k] != '%')
 		{
-			_putchar(format[i]);
+			_putchar(format[k]);
 			my_chars++;
 		}
 		else
 		{
-			i++;
-			if (format[i] == '\0')
-			{
+			k++;
+			if (format[k] == '\0')
 				break;
-			}
-			if (format[i] == '%')
+			if (format[k] == '%')
 			{
-				_putchar(format[i]);
+				_putchar(format[k]);
 				my_chars++;
 			}
-			else if (format[i] == 'c')
+			else if (format[k] == 'c')
 			{
-				char p = va_arg(args, int);
-
-				_putchar(p);
+				_putchar(va_arg(num_of_args, int));
 				my_chars++;
 			}
-			else if (format[i] == 's')
+			else if (format[k] == 's')
 			{
-				char *my_string = va_arg(args, char *);
+				char *my_string = va_arg(num_of_args, char *);
 
 				length_of_string = 0;
 
@@ -58,38 +46,14 @@ int _printf(const char *format, ...)
 				}
 				my_chars += length_of_string;
 			}
-			else if (format[i] == 'd' || format[i] == 'i')
-			{
-				int p = va_arg(args, int);
-
-				printint(p);
-				my_chars++;
-			}
 			else
 			{
 				_putchar('%');
-				_putchar(format[i]);
+				_putchar(format[k]);
 				my_chars += 2;
 			}
 		}
 	}
-	va_end(args);
+	va_end(num_of_args);
 	return (my_chars);
-}
-void printint(int p)
-{
-	if (p < 0)
-	{
-		_putchar('-');
-		p = -p;
-	}
-	if (p == '0')
-	{
-		_putchar('0');
-	}
-	if (p >= 10)
-	{
-		printint(p / 10);
-	}
-	_putchar(p % 10 + '0');
 }
